4a09eb9749474accef7fe0d05ba2515d7e265c3e
DangoPlus <depengyu@gmail.com>
2021-08-05 15:49:09 +0800

    Update utils.js
    更新一个解决跨域文件名无法生效的下载方法

--- utils.js
+++ utils.js
@@ -66,3 +66,23 @@ export const downloadFile = (url: string, filename = '') => {
   a.click();
   window.URL.revokeObjectURL(url);
 };
+
+//解决跨域文件名无法生效的下载方法
+export const downloadFileAlt = (url: string, filename = '') => {
+  var x = new XMLHttpRequest();
+      x.open("GET", url, true);
+      x.responseType = "blob";
+      x.onload = function(e) {
+        //会创建一个 DOMString，其中包含一个表示参数中给出的对象的URL。这个 URL 的生命周期和创建它的窗口中的 document 绑定。这个新的URL 对象表示指定的 File 对象或 Blob 对象。
+        var url = window.URL.createObjectURL(x.response);
+        var a = document.createElement("a");
+        a.href = url;
+        a.download = filename;
+        a.click();
+      };
+      x.send();
+};
+
+
+
+
